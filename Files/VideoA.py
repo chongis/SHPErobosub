5a1be23175118b43e3119951330eaaa9558a5738
bcid <bcid@uci.edu>
2019-02-20 14:52:16 -0800

    bcid

--- Files/VideoA.py
+++ Files/VideoA.py
@@ -14,22 +14,24 @@ def resize_image(a):
     hsize = int((float(img.size[1])*float(wpercent)))
     img = img.resize((basewidth,hsize), Image.ANTIALIAS)
     img.save(a)
+    return img
     
 
-def send_resized_image(a):
-    #a is a string naming a path to the location of the image
+def send_resized_image(image_path):
+    #image_path is a string naming a path to the location of the image
     import socket
 
-    img = resize_image(a)
+    img = resize_image(image_path)
     HOST = '127.0.0.1'
     PORT = 3333
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        filein = os.open("./bigtest.txt",os.O_RDONLY);
+        filein = os.open(image_path, os.O_RDONLY)
+        #not sure why it's 4069
         sent = os.sendfile(s.fileno(),filein,None,4069)
         while(sent):
-            sent = os.sendfile(s.fileno(),filein,None,4069)
+            sent = os.sendfile(s.fileno(),filein,None,4069) 
         os.close(filein)
         s.close()
         
